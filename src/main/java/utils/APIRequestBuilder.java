b9a632c6f2804e886228173e3064b46c334719e4
FirstRay1 <raysergei@yandex.ru>
2019-12-16 03:29:51 +0300

    test
    test

---
+++ src/main/java/utils/APIRequestBuilder.java
@@ -0,0 +1,28 @@
+package utils;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+public class APIRequestBuilder {
+    public static RequestSpecification SetDefaultRequest() {
+//API basepath
+        RestAssured.baseURI = "https://api.weather.yandex.ru/v1/forecast";
+//Key for testing Yandex Forecast
+        String headerKey = "X-Yandex-API-Key";
+        String headerValue = "72b29110-0527-4d78-85bd-d0e8a27a65bb";
+//office shopping center Vodnii
+        String testLat = "55.839906";
+        String testLon = "37.490297";
+        String testHours ="hours=false";  //to get the most simple answer
+
+        RequestSpecBuilder builder = new RequestSpecBuilder();
+        builder.addHeader(headerKey, headerValue);
+        builder.addParam(testLat);
+        builder.addParam(testLon);
+        builder.addParam(testHours);
+        RequestSpecification requestSpec = builder.build();
+        return requestSpec;
+    }
+
+}
---
+++ src/test/java/getforecast/TestAPIYandexForecast.java
@@ -0,0 +1,50 @@
+package getforecast;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import org.testng.annotations.Test;
+import utils.APIRequestBuilder;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class TestAPIYandexForecast {
+
+    @Test
+    public void CheckAuthorization(){
+        given().
+        spec(APIRequestBuilder.SetDefaultRequest()).
+        when().
+        get().
+        then().
+        statusCode(200);
+
+        System.out.printf("\nDefaultParamsAuthorized\n");
+    }
+    @Test
+    public void CheckDefaultJSONSchema(){
+        ValidatableResponse response = given().
+        spec(APIRequestBuilder.SetDefaultRequest()).
+        when().
+        get().
+        then().
+        contentType(ContentType.JSON).assertThat().
+        body(matchesJsonSchemaInClasspath("default.json"));
+        String responseBody = response.extract().asString();
+        System.out.printf("\nDefaultDefaultJSONSchemaChecked\n");
+    }
+/*TODO
+!Logging
+Check for:
+1. Authorization with wrong header
+2. Request without obligatory params
+3. JSONSchema with "hours"
+4. JSONSchema with "extra"
+5. "info.url" for all "lang"
+6. Requests with different "limits" (valid/invalid)
+7. Correlation between "now" and "now_dt"
+8. Correlation "info.lat", "info.lat" between request params
+
+ */
+
+}
