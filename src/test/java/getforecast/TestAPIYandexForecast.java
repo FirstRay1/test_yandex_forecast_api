0a7c27d0c022ee7efffbcbb731ea913f5eabfbb6
FirstRay1 <raysergei@yandex.ru>
2019-12-21 22:37:24 +0300

    DueToLangURLResponse check added

--- src/test/java/getforecast/TestAPIYandexForecast.java
+++ src/test/java/getforecast/TestAPIYandexForecast.java
@@ -2,6 +2,8 @@ package getforecast;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import utils.APIRequestBuilder;
 
@@ -73,6 +75,34 @@ public class TestAPIYandexForecast {
         String responseBody = response.extract().asString();
         System.out.printf("\nDefaultNoHoursWithExtraJSONSchema checked\n");
     }
+    //DataProvider contains pairs for request param "lang" and response "info.URL" due to
+    // specs from https://yandex.ru/dev/weather/doc/dg/concepts/forecast-test-docpage/
+    @DataProvider(name = "testLang")
+    public Object[][] createData1() {
+        return new Object[][] {
+                { "ru_RU", "yandex.ru/pogoda"},
+                { "ru_UA", "yandex.ua/pogoda"},
+                { "uk_UA", "yandex.ua/pogoda"}, //In Russia equal to ru_UA
+                { "be_BY", "yandex.by/pogoda"},
+                { "kk_KZ", "yandex.kz/pogoda"},
+                { "tr_TR", "yandex.com.tr/hava"},
+                { "en_US", "yandex.com/pogoda/"},
+        };
+    }
+    //Test for parameter "lang" from
+    @Test(dataProvider = "testLang")
+    public void CheckLangURL(String paramLang, String objectURL) {
+        ValidatableResponse response = given().
+                spec(APIRequestBuilder.SetDefaultRequest()).
+                param("lang", paramLang).
+                when().
+                get().
+                then().
+                contentType(ContentType.JSON).assertThat();
+        String responseURL = response.extract().jsonPath().getString("info.url");
+        Assert.assertTrue(responseURL.contains(objectURL));
+        System.out.printf("\nDueToLangURLResponse checked\n");
+    }
 
 
 /*TODO
@@ -84,8 +114,9 @@ Spec(https://yandex.ru/dev/weather/doc/dg/concepts/forecast-test-docpage/) error
 with some data. So there are no obligatory params
 3. JSONSchema with "hours"                                    --> DONE
 4. JSONSchema with "extra"                                    --> DONE
-5. "info.url" for all "lang"
-6. Requests with different "limit" (valid/invalid, check schema correlation to limit value)
+5. "info.url" for all "lang"                                  --> DONE
+6. Requests with different "limit"
+(valid/invalid, check schema correlation to limit value)
 7. Correlation between "now" and "now_dt"                     --> DONE TestRequestedParamsIndependentResponses
 8. Correlation "info.lat", "info.lon" between request params  --> Failing test
  */
